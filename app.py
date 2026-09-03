dfedcb15e03cc0ce59b9848a3a72f1387bf265da
Ahalya <ahalyar4328@gmail.com>
2026-09-03 21:23:31 +0530

    Fix Flask Vercel deployment

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from app import app
+from flask import Flask
+
+app = Flask(__name__) 
 import os
 from flask import Flask, flash, redirect, render_template, request, url_for, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
